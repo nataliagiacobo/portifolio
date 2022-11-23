ed872e2cfd442d194d1be2adfce03e9017700721
Natalia Rohr <natalia.giacobo@gmail.com>
2022-11-23 16:35:34 -0300

    Desenvolvimento Portifólio

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-var prevScrollpos = window.pageYOffset;
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//     console.log("rodando");
+//     var currentScrollPos = window.pageYOffset;
+//     if (prevScrollpos > currentScrollPos) {
+//         document.getElementById("myNavbar").style.top = "0";
+//     } else {
+//         document.getElementById("myNavbar").style.top = "-65px";
+//     }
+//     prevScrollpos = currentScrollPos;
+// }
+
+var prevScrollPos = window.pageYOffset;
 window.onscroll = function () {
+    console.log("rodando");
     var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("myNavbar").style.top = "0";
-    } else {
-        document.getElementById("myNavbar").style.top = "-65px";
+    if (prevScrollPos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+        prevScrollPos = currentScrollPos;
+    } else if (currentScrollPos > prevScrollPos + 200) {
+        document.getElementById("navbar").style.top = "-73px";
+        prevScrollPos = currentScrollPos;
     }
-    prevScrollpos = currentScrollPos;
 }
