872dac4a7237a7b1e5ec096d2613ff3d5bbebc7e
Natalia Rohr <natalia.giacobo@gmail.com>
2022-11-23 17:35:21 -0300

    Portifolio

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function () {
-//     console.log("rodando");
-//     var currentScrollPos = window.pageYOffset;
-//     if (prevScrollpos > currentScrollPos) {
-//         document.getElementById("myNavbar").style.top = "0";
-//     } else {
-//         document.getElementById("myNavbar").style.top = "-65px";
-//     }
-//     prevScrollpos = currentScrollPos;
-// }
 
 var prevScrollPos = window.pageYOffset;
 window.onscroll = function () {
@@ -21,3 +10,4 @@ window.onscroll = function () {
         prevScrollPos = currentScrollPos;
     }
 }
+
